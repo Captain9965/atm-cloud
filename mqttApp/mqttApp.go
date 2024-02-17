f9b021c098de9a8d3b186c1e59ad7925aea9cae3
lenny <lennyweda@gmail.com>
2024-02-17 17:05:16 +0300

    (master): process server hello event with one mqtt client

--- mqttApp/mqttApp.go
+++ mqttApp/mqttApp.go
@@ -2,16 +2,32 @@ package mqttApi
 
 import (
 	"fmt"
+	"strings"
 
 	"log"
 
 	"time"
 
 	mqtt "github.com/eclipse/paho.mqtt.golang"
+
+	"encoding/json"
 )
 
-func DisplayMqtt() {
-	fmt.Println("Launching mqtt app")
+var mqttClient *mqtt.Client
+
+type event_t struct {
+	Ev string `json:"ev"`
+}
+
+type hello_t struct {
+	Ms  string `json:"ms"`
+	Ver string `json:"ver"`
+}
+
+func RunMqtt() {
+	NewClient := Connect("master", "broker.hivemq.com:1883", "lenny", "Lenny123")
+	mqttClient = &NewClient
+	Listen(mqttClient, "w/s/#")
 }
 
 func Connect(clientId string, uri string, username string, password string) mqtt.Client {
@@ -36,11 +52,58 @@ func CreateClientOptions(clientId string, uri string, username string, password 
 	return opts
 }
 
-func Listen(uri string, topic string, clientId string, username string, password string) {
-	client := Connect(clientId, uri, username, password)
-	client.Subscribe(topic, 0, func(client mqtt.Client, msg mqtt.Message) {
-		fmt.Printf("* [%s] %s\n", msg.Topic(), string(msg.Payload()))
+func Listen(client *mqtt.Client, topic string) {
+	(*client).Subscribe(topic, 0, func(client mqtt.Client, msg mqtt.Message) {
+		// log mqtt message:
+		log.Printf("* [%s] %s\n", msg.Topic(), string(msg.Payload()))
+		go handleMessage(msg.Payload(), msg.Topic())
 	})
 
 	fmt.Println("Subscription successful")
+}
+
+func handleMessage(message []byte, topic string) {
+	var event event_t
+	if err := json.Unmarshal(message, &event); err != nil {
+		log.Printf("%s", err.Error())
+	}
+
+	// Handle different Event types:
+	switch event.Ev {
+	case "hello":
+		var helloData hello_t
+		if err := json.Unmarshal(message, &helloData); err != nil {
+			log.Printf("%s", err.Error())
+			return
+		}
+		handleHelloEvent(&helloData, topic)
+	default:
+		log.Printf("%s Event not supported", event.Ev)
+
+	}
+}
+
+func handleHelloEvent(data *hello_t, topic string) {
+	//get settings for different machine versions:
+	switch data.Ver {
+		case "1.0.0":
+			deviceUid, ok := getUidFromTopic(topic)
+			if !ok {
+				deviceUid = "None"
+				log.Printf("Unable to get uid from %s \n", topic)
+			}
+			(*mqttClient).Publish("w/s/"+deviceUid, 0, false, `{"ev":"config","admin id":"5678","vending id":"5783","service id":"8990","cash":"6789","taps":"1111"}`)
+		default:
+			log.Printf("%s not supported", data.Ver)
+
+	}
+}
+
+func getUidFromTopic(topic string) (string, bool) {
+	parts := strings.Split(topic, "/")
+	if len(parts) > 1 {
+		return parts[len(parts)-1], true
+	} else {
+		return "", false
+	}
 }
