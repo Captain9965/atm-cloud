dc7f2bacbb5604b4384183e37cbf404eb860c2f5
lenny <lennyweda@gmail.com>
2024-03-27 23:15:00 +0300

    (f/postgresdb): models defined, not yet refined

--- dbApp/dbApp.go
+++ dbApp/dbApp.go
@@ -1,38 +1,263 @@
 package dbApp
 
 import (
-	"database/sql"
+	"encoding/json"
 	"fmt"
-	"github.com/joho/godotenv"
-	_ "github.com/lib/pq"
+	// "github.com/gin-gonic/gin"
+	"github.com/google/uuid"     // Import for UUID generation
+	"github.com/joho/godotenv"   // Import for loading environment variables
+	"golang.org/x/crypto/bcrypt" // Import for password hashing
+	"gorm.io/gorm"
+	"gorm.io/driver/postgres"
 	"os"
-	"strconv"
+	"time"
 )
 
-var Db *sql.DB // DB instance
+type GormDB struct {
+	*gorm.DB //gorm.DB object
+  }
 
-func ConnectDatabase() {
-	err := godotenv.Load() // load .env by default
-	if err != nil {
-		fmt.Println("Cannot load DB environment variables")
-		// just crash
-		panic(err)
-	}
-	host := os.Getenv("HOST")
-	port, _ := strconv.Atoi(os.Getenv("PORT")) // don't forget to convert int since port is int type.
-	user := os.Getenv("USER")
-	dbname := os.Getenv("DB_NAME")
-	pass := os.Getenv("PASSWORD")
+type User struct {
+	gorm.Model
+	Username       string       `json:"username"`
+	UserPhone      int          `json:"user_phone_number"`         // Optional field
+	MachinesOwned  []Machine    `gorm:"foreignKey:OwnerID"`  //one 2 many
+	Role           int          `json:"role"`                      // Permission level (0 - Superuser, 1 - Admin, 2 - Owner, 3 - Operator)
+	OrganizationID uint         `json:"organization_id"`           // Foreign key to Organizations
+	Organization   Organization `gorm:"foreignKey:OrganizationID"` // Belongs to Organization
+	Password string       		`json:"-"`                         // Excluded from JSON marshaling
+}
 
-	// set up postgres sql to open it.
-	psqlSetup := fmt.Sprintf("host=%s port=%d user=%s dbname=%s password=%s sslmode=disable",
-	host, port, user, dbname, pass)
-	db, err := sql.Open("postgres", psqlSetup)
+type Organization struct {
+	gorm.Model
+	OrganizationName 	string  `json:"organization_name"`         //name of organisation
+	OrganizationID		int    `json:"organization_id"`           // Primary key
+	Users          		[]User `gorm:"foreignKey:OrganizationID"` // One-to-Many relationship with Users
+}
+
+type Machine struct {
+	gorm.Model
+	MachineType           string          `json:"machine_type"`
+	MachineID             string          `json:"machine_id"`
+	OwnerID               uint            `json:"owner_id"`     // foreign key to Users 
+	User                  User            `gorm:"foreignKey:OwnerID"` // belongs to User 
+	AdminCardID           string          `json:"admin_card_id"`
+	VendingCardID         string          `json:"vending_card_id"`
+	ServiceCardID         string          `json:"service_card_id"`
+	MachineLastUpdateTime time.Time       `json:"machine_last_update_time"`
+	TapEnableStatus       int             `json:"tap_enable_status"`
+	AdminCash             int             `json:"admin_cash"`
+	Rss                   int             `json:"rss"`                  // Signal strength
+	Locations             json.RawMessage `json:"locations"`            // Cell tower location as JSON
+	Transactions          []Transactions  `gorm:"foreignKey:MachineID"` // One-to-Many relationship with Transactions
+}
+
+type Transactions struct {
+	gorm.Model
+	MachineID         uint      `json:"machine_id"`           // Foreign key to Machine
+	Machine           Machine   `gorm:"foreignKey:MachineID"` // Belongs to Machine
+	TransactionType   int       `json:"transaction_type"`
+	Amount            int       `json:"amount"`
+	TransactionUUID   uuid.UUID `gorm:"type:uuid"`
+	TransactionCode   string    `json:"transaction_code"`                     // Optional field
+	TransactionStatus int       `json:"transaction_status"`
+}
+
+type Database interface {
+	// Connection management
+	Connect() error  // Connects to the database
+	//users
+	CreateUser(userData map[string]interface{}) error  // Creates a new user
+  	GetUserByName(username string) (map[string]interface{}, error)    // Retrieves user details by name (returns a map)
+  	UserExists(username string) bool //method to check user existence
+  	UpdateUserByName(username string, fieldsToUpdate map[string]interface{}) error //method to update by name
+  	DeleteUserByName(username string) error                        // Deletes a user by name
+	GetAllUsers() ([]map[string]interface{}, error)
+  
+  }
+
+  func (db *GormDB) Connect() error {
+	err := godotenv.Load() // Load environment variables from .env file (optional)
 	if err != nil {
-		fmt.Println("There is an error while connecting to the database ", err)
-		panic(err)
-	} else {
-		Db = db
-		fmt.Println("Successfully connected to database!")
+	  return fmt.Errorf("error loading environment variables: %w", err)
 	}
+  
+	dbUsername := os.Getenv("DB_USERNAME")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+  
+	// Build the connection string using environment variables
+	connectionString := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", dbHost, dbUsername, dbPassword, dbName, dbPort)
+  
+	// Configure connection options (optional)
+	config := &gorm.Config{}
+	// Add other configuration options that can be made: 
+	// config.Logger = logger.Default // Set a custom logger
+  
+	dbClient, err := gorm.Open(postgres.Open(connectionString), config)
+	if err != nil {
+	  return fmt.Errorf("failed to connect to database: %w", err)
+	}
+  
+	// Set automigrations mode (optional)
+	err = dbClient.AutoMigrate(&User{}, &Organization{},&Machine{},&Transactions{})
+	if err != nil{
+		fmt.Println("Error occured during migration: ",err)
+	}
+
+	db.DB = dbClient
+	return nil
+}
+
+// check if a user exists: 
+func (db *GormDB) UserExists(username string) bool {
+
+	var user User
+	result := db.DB.Where("username = ?", username).First(&user)
+	return result.Error == nil && result.RowsAffected > 0
+}
+
+func (db *GormDB)GetUserByName(username string) (map[string]interface{}, error){
+
+	if !db.UserExists(username) {
+		return nil, fmt.Errorf("user with username '%s' does not exist", username)
+	  }
+	var user User
+	result := db.DB.Where("username = ?", username).First(&user)
+	if result.Error != nil {
+	  return nil, result.Error
+	}
+
+	userData := map[string]interface{}{
+	"username": user.Username,
+	"phone_number": user.UserPhone,
+	"role":     user.Role,
+	}
+
+	return userData, nil
+
+}
+
+// update user details: 
+func (db *GormDB) UpdateUserByName(username string, fieldsToUpdate map[string]interface{}) error {
+
+	if !db.UserExists(username) {
+	  return fmt.Errorf("user with username '%s' does not exist", username)
+	}
+  
+	// Get existing user data
+	var existingUser User 
+	result := db.DB.Where("username = ?", username).First(&existingUser)
+	if result.Error != nil {
+	  return result.Error
+	}
+  
+	// Update user fields based on the provided map
+	for field, newValue := range fieldsToUpdate {
+	  switch field {
+	  case "username": 
+		existingUser.Username = newValue.(string)
+	  case "password":
+		hashedPassword, err := HashPassword(newValue.(string))
+		if err != nil {
+		  return err
+		}
+		existingUser.Password = hashedPassword
+	  case "role":
+		existingUser.Role = newValue.(int) // Ensure newValue is cast to int
+	  case "phone_number": // Assuming a single phone number field in User struct
+		existingUser.UserPhone = newValue.(int)
+	  default:
+		// Handle updates for other supported fields (if any)
+	  }
+	}
+  
+	result = db.DB.Save(&existingUser)
+	if result.Error != nil {
+	  return result.Error
+	}
+	return nil
+  }
+
+//create new user	
+func (db *GormDB) CreateUser(userData map[string]interface{}) error {
+
+	// Validate and extract data from the map
+	username, ok := userData["username"].(string)
+	if !ok || username == "" {
+	  return fmt.Errorf("missing or invalid username in user data")
+	}
+  
+	password, ok := userData["password"].(string)
+	if !ok || password == "" {
+	  return fmt.Errorf("missing or invalid password in user data")
+	}
+  
+	role, ok := userData["role"].(int)
+	if !ok {
+	  return fmt.Errorf("missing or invalid role in user data")
+	}
+  
+	hashedPassword, err := HashPassword(password)
+	if err != nil {
+	  return err
+	}
+  
+	// Check if user with the same username already exists
+	if db.UserExists(username) {
+		return fmt.Errorf("User with username '%s' already exists", username)
+	}
+	
+	// User doesn't exist, create directly using model
+	newUser := User{Username: username, Password: hashedPassword, Role: role}
+	result := db.DB.Create(&newUser)
+	if result.Error != nil {
+	  return result.Error
+	}
+	return nil
+}
+
+func (db *GormDB) DeleteUserByName(username string) error {
+	if !db.UserExists(username) {
+	  return fmt.Errorf("user with username '%s' does not exist", username)
+	}
+  
+	result := db.DB.Where("username = ?", username).Delete(&User{}) 
+	if result.Error != nil {
+	  return result.Error
+	}
+  
+	return nil
+}
+
+func (db *GormDB) GetAllUsers() ([]map[string]interface{}, error) {
+	var users []User 
+	result := db.DB.Find(&users)
+	if result.Error != nil {
+	  return nil, result.Error
+	}
+  
+	// Convert user structs to maps
+	userData := make([]map[string]interface{}, len(users))
+	for i, user := range users {
+	  userData[i] = map[string]interface{}{
+		"username": user.Username,
+		"phone_number": user.UserPhone,
+		"role":     user.Role,
+	  }
+	}
+  
+	return userData, nil
+  }
+
+// HashPassword function to hash passwords using bcrypt
+func HashPassword(password string) (string, error) {
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(hashedPassword), nil
 }
